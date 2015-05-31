1b2d72135f21fbf28155f9b4a234cc860da98b5b
Dudy <earthami5@gmail.com>
2015-05-31 09:23:47 +0800

    update

--- src/js/practice-1.js
+++ src/js/practice-1.js
@@ -64,10 +64,10 @@ D1410291Fn();
 //    alert(document.body.scrollHeight);
 //    alert(document.body.offsetHeight);
 
-function consoleLog(Content) {
-    if (!window.console) return false;
-    console.log(Content);
-    }
+//function consoleLog(Content) {
+//    if (!window.console) return false;
+//    console.log(Content);
+//    }
 
 //    log(document.getElementById("D-141031-2").clientLeft);
 //    consoleLog("fdsafdv");
@@ -1081,40 +1081,40 @@ FnD1411012();*/
 //})();
 
 
-(function(){
-
-    var h = document.getElementById("D-150506-1");
-    var txt = h.firstChild;
-    var range = document.createRange();
-    range.setStart(txt,10);
-    range.setEnd(txt,271);
-    range.collapse(true);
-
-
-})();
-
-(function(){
-
-    var wrap = document.getElementById("D-150509-2");
-    var eles = wrap.getElementsByTagName("div");
-
-    for(var i=0; i<eles.length; i++){
-        eles[i].onclick = function(event){
-            alert2(event);
-        }
-    }
-
-    document.getElementsByTagName("body")[0].onclick = function(event){
-        alert2(event);
-    }
-
-    function alert2(event){
-        if(event.target == event.currentTarget){
-            console.log(event.currentTarget);
-        }
-    }
-
-})();
+//(function(){
+//
+//    var h = document.getElementById("D-150506-1");
+//    var txt = h.firstChild;
+//    var range = document.createRange();
+//    range.setStart(txt,10);
+//    range.setEnd(txt,271);
+//    range.collapse(true);
+//
+//
+//})();
+//
+//(function(){
+//
+//    var wrap = document.getElementById("D-150509-2");
+//    var eles = wrap.getElementsByTagName("div");
+//
+//    for(var i=0; i<eles.length; i++){
+//        eles[i].onclick = function(event){
+//            alert2(event);
+//        }
+//    }
+//
+//    document.getElementsByTagName("body")[0].onclick = function(event){
+//        alert2(event);
+//    }
+//
+//    function alert2(event){
+//        if(event.target == event.currentTarget){
+//            console.log(event.currentTarget);
+//        }
+//    }
+//
+//})();
 
 //(function(){
 //
@@ -1138,15 +1138,20 @@ FnD1411012();*/
 //
 //})();
 
-(function(){
+//(function(){
+//
+//    var ele = document.getElementById("D-150515-2");
+//    console.log(ele.childNodes.length);
+//
+//})();
 
-    var ele = document.getElementById("D-150515-2");
-    console.log(ele.childNodes.length);
+//window.onload = function(){
+//    alert("onload");
+//};
 
-})();
-
-
-
+window.addEventListener("unload",function(){
+   alert("onunload");
+});
 
 
 
